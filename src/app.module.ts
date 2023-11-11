02ea0503a9ac61e5459d9c5accb1f32ecfbe65d7
gamenarkyt <gagaritin11@gmail.com>
2023-11-11 18:38:32 +0300

    Refactor appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService, PrismaService],
-  exports: [PrismaService]
+  imports: [AuthModule, UsersModule, PrismaModule],
+  controllers: [AppController],
+  providers: [AppService],
+  exports: [PrismaModule]
 })
 export class AppModule {}
+
+
+// controllers: [AppController, UsersController],
+// providers: [AppService, UsersService, PrismaService],
+// exports: [PrismaService]
